76d91bc28d9998ffbd4f97b2c43a6ed0f76fc53d
Gérald Barré <meziantou@live.fr>
2021-06-12 11:25:24 -0400

    Add CI

--- XUnitToFluentAssertionsAnalyzer/XUnitToFluentAssertionsAnalyzer/Extensions.cs
+++ XUnitToFluentAssertionsAnalyzer/XUnitToFluentAssertionsAnalyzer/Extensions.cs
@@ -11,7 +11,7 @@ namespace XUnitToFluentAssertionsAnalyzer
 
         public static bool IsOrImplements(this ITypeSymbol symbol, Compilation compilation, string type)
         {
-            INamedTypeSymbol otherSymbol = compilation.GetTypeByMetadataName(type);
+            var otherSymbol = compilation.GetTypeByMetadataName(type);
             if (symbol.Equals(otherSymbol, SymbolEqualityComparer.Default))
                 return true;
 
